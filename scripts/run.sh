1bedd8f69dcfad061a1ff6bdc8f021077e5e6b98
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-30 20:27:31 -0300

    feat(docker-compose): volumes entre containers.

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,7 +11,7 @@ python manage.py check
 
 # echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 
-# python manage.py collectstatic
+python manage.py collectstatic --clear
 
 # coverage html -d htmlcov
 
